63be42eeb01369121b8905cbf50529535a8f42a3
Ivan Belitskii <i.belitskiy@redmond-rus.com>
2016-02-02 19:37:24 +0300

    hot fix

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -11,66 +11,7 @@
         'novalidate' => 'novalidate',
         'files' => true)) !!}
 
-<div class="form-group">
-    {!! Form::label('Название') !!}
-    {!! Form::text('name', null, array('placeholder'=>'name', 'class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Символьный код') !!}
-    {!! Form::text('code', null, array('placeholder'=>'code', 'class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-    <div class="checkbox">
-        <label>
-            {!! Form::checkbox('active', 1, true) !!} <b>Активность</b>
-        </label>
-    </div>
-</div>
-<div class="form-group">
-    {!! Form::label('Начало активности') !!}
-    {!! Form::input('date', 'active_from', null, array('class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Окончание активности') !!}
-    {!! Form::input('date', 'active_till', null, array('class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Сортировка') !!}
-    {!! Form::text('sort', null, array('placeholder'=>'500', 'class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Картинка для анонса') !!}
-    {!! Form::file('preview_picture', null) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Описание для анонса') !!}
-    {!! Form::textarea('preview_text', null, array('class' => 'form-control', 'rows' => 3)) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Детальная картинка') !!}
-    {!! Form::file('detail_picture', null) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Детальное описание') !!}
-    {!! Form::textarea('detail_text', null, array('class' => 'form-control', 'rows' => 3)) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('XML ID') !!}
-    {!! Form::text('xml_id', null, array('placeholder'=>'', 'class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Meta Title') !!}
-    {!! Form::text('meta_title', null, array('placeholder'=>'title', 'class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Meta Keywords') !!}
-    {!! Form::text('meta_keywords', null, array('placeholder'=>'keywords', 'class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('Meta Description') !!}
-    {!! Form::text('meta_description', null, array('placeholder'=>'description', 'class' => 'form-control')) !!}
-</div>
-
+@include('admin/news/_form')
 
 
 
