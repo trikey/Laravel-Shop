96d857c1e0f83ab6208e2f220b41ca7be065c8b0
Ivan Belitskii <i.belitskiy@redmond-rus.com>
2016-02-04 10:35:25 +0300

    how to make cart stored in comments

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,16 +10,36 @@ class CartController extends Controller {
     public function addToCart(Request $request)
     {
         dd($request->all());
+
+        /*
+        if session.products
+            if session.products[product_id]
+                session.products[product_id] += quantity
+            else
+                session.products[product_id] = quantity
+        else
+            session.products = []
+            session.products[product_id] = quantity
+        */
     }
 
     public function updateCart(Request $request)
     {
-
+        /*
+         * if session.products
+				if session.products[basket_id]
+					session.products[basket_id] = quantity
+				else
+					session.products[basket_id] = quantity
+			else
+				session.products = []
+				session.products[basket_id] = quantity
+         */
     }
 
     public function deleteFromCart(Request $request)
     {
-
+        // delete session.products[basket_id]
     }
 
     public function getSmallCart(Request $request)
