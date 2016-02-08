917d88c9435d8e074eefd8dc94ea31fd4278f909
redmond <5iFUu6SRAsAb0uS5>
2016-02-08 08:53:40 +0400

    order making started

---
+++ database/migrations/2016_02_06_155000_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('orders', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('total_price')->nullable();
+			$table->string('delivery_price')->nullable();
+			$table->integer('pay_system_id')->unsigned()->nullable();
+			$table->integer('delivery_id')->unsigned()->nullable();
+			$table->string('xml_id')->nullable();
+			$table->integer('modified_by')->unsigned();
+			$table->timestamps();
+
+			$table->foreign('modified_by')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('orders');
+	}
+
+}
